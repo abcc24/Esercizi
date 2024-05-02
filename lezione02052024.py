41805ede35b5414fe64f4d70be34a0ad0b79057a
abcc24 <dirge20@gmail.com>
2024-05-02 16:03:34 +0200

    nuova lezione

---
+++ lezione02052024.py
@@ -0,0 +1,2 @@
+#Scrivi una funzione che ritorna il valore massimo,
+#minimo e la media di una lista di numeri interi.
---
+++ loot.py
@@ -0,0 +1,4 @@
+def loot():
+    items: list = ["dagger", "sword", "helm", "armor"]
+    for x in items:
+        print(x)
