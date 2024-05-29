84f533af0c5c6ed4476439df2ae0d67c06a99689
Alberto <159887450+abcc24@users.noreply.github.com>
2024-05-29 19:10:23 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+import unittest
+from calc import Calculations
+
+class TestCalc(unittest.TestCase):
+    def test_sum(self):
+        calc_1 = Calculations (a=2, b=3)
+        result = calc_1.get_sum()
+        self.assertEqual(result, 5, msg = f"Error Test Failed {result}")
+
+
+if __name__ == "__main__":
+    unittest.main()
