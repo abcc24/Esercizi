2b586ad40efb34852df75964b2df092bc22b6ec0
abcc24 <oblivion9124@gmail.com>
2024-06-21 13:40:17 +0200

    b

---
+++ PlusOne.py
@@ -0,0 +1,18 @@
+digits = [1,2,3,9]
+
+"""lastN = digits.pop()
+
+lastNu = lastN + 1
+        
+if lastNu < 10:
+    digits.append(lastNu)
+    print (digits)
+
+else:
+    NUM = [int(x) for x in str(lastNu)]
+    digits.extend(NUM)
+    print(digits)"""
+
+x = map(int, str(digits))
+
+print(x)
