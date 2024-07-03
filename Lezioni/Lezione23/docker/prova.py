3f14b200cf9e24c8bdbb4594dcc1d544ddf19018
abcc24 <oblivion9124@gmail.com>
2024-07-03 11:58:39 +0200

    update

---
+++ Lezioni/Lezione23/docker/prova.py
@@ -0,0 +1,8 @@
+pizze: list=["diavola","marinara","napoli"]
+for x in pizze:
+    print(x)
+"""I made a list of pizzas, the I did a loop to print all the pizzas"""
+for x in pizze:
+    print(f"I like pizza {x}.")
+"""I write a sentence for each type of pizza"""
+print(f"I adore the flavour of {x}")
